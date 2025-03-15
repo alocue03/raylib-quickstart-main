e283f03e3d949dbf3b08837b1cfbabf829c235a9
alocue03 <60159542+alocue03@users.noreply.github.com>
2025-03-15 07:01:43 -0600

    main.lua

---
+++ bin/Debug/main.lua
@@ -0,0 +1,9 @@
+local simpledraw = require "SimpleDraw"
+
+function Draw (dt)
+
+
+	simpledraw.DrawCircle(100,400,25,45,100,149,0)
+	simpledraw.DrawRect(200,200,200,55,0,255,255,0)
+	--SimpleDraw.Clear(255,200,200)
+end
